6a1d59271a8112f9cd4c5eb1ae262fec1e58f3df
Jasmine Fan <jf4654@nyu.edu>
2026-08-08 18:46:18 -0400

    on task 4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,19 +13,37 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let bookStr = JSON.stringify(books);
+  return res.status(200).send(bookStr);
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn;
+  let book = books[isbn]
+
+  if (book) {
+    return res.status(200).send(book);
+  } else {
+    return res.status(404).json({ message: "Book not found for IBSN" });
+  }
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let author = req.params.author
+  const bookKeys = Object.keys(books)
+  // need to get multiple books
+  let filteredBooks = bookKeys.filter((isbn) => {
+    return (books[isbn].author === author)
+  })
+  // convert isbns into proper arr
+  filteredBooks.forEach((isbn, index) => {
+    filteredBooks[index] = books[isbn]
+  })
+  return res.status(200).send(filteredBooks);
 });
 
 // Get all books based on title
