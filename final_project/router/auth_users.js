30922475dc66d964b4cff8344ab230a209474061
Jasmine Fan <jf4654@nyu.edu>
2026-08-09 00:21:59 -0400

    done w task 9

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -51,9 +51,35 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+    const isbn = req.params.isbn;
+    const review = req.query.review;
+    const username = req.session.authorization.username;
+  
+    const book = books[isbn];
+    if (!book) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+    if (!review) {
+      return res.status(400).json({ message: "Review text is required as a query parameter" });
+    }
+  
+    book.reviews[username] = review;
+    return res.status(200).json({ message: "Review successfully added/updated", reviews: book.reviews });
+  });
+
+regd_users.delete("/auth/review/:isbn", (req,res) => {
+    const isbn = req.params.isbn;
+    const username = req.session.authorization.username;
+  
+    const book = books[isbn];
+    if (!book) {
+        return res.status(404).json({ message: "Book not found" });
+    }
+
+    delete book.reviews[username];
+
+    return res.status(200).json({message: "Review successfully deleted", reviews: book.reviews});
+})
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
